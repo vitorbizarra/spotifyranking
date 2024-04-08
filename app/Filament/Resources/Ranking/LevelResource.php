09ce7d80e7ffeef0956d342542f49e1a0c3d6e9d
vitorbizarra <vitorbizarracontato@gmail.com>
2024-04-08 20:32:22 -0300

    :sparkles: feat: LevelResource

---
+++ app/Filament/Resources/Ranking/LevelResource.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Filament\Resources\Ranking;
+
+use App\Filament\Resources\Ranking\LevelResource\Pages;
+use App\Filament\Resources\Ranking\LevelResource\RelationManagers;
+use App\Models\Ranking\Level;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Support\Enums\MaxWidth;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class LevelResource extends Resource
+{
+    protected static ?string $model = Level::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-star';
+
+    protected static ?string $modelLabel = 'nível';
+
+    protected static ?string $pluralModelLabel = 'níveis';
+
+    protected static ?string $recordTitleAttribute = 'title';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->columns(1)
+            ->schema([
+                Forms\Components\TextInput::make('title')
+                    ->label('Título')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\ColorPicker::make('color')
+                    ->label('Cor')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->reorderable('sort')
+            ->defaultSort('sort')
+            ->columns([
+                Tables\Columns\TextColumn::make('title')
+                    ->label('Título')
+                    ->searchable(),
+                Tables\Columns\ColorColumn::make('color')
+                    ->label('Cor'),
+                Tables\Columns\TextColumn::make('sort')
+                    ->label('Posição')
+                    ->numeric()
+                    ->sortable()
+                    ->badge(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->label('Criado em')
+                    ->dateTime('d/m/Y H:i')
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->label('Atualizado em')
+                    ->dateTime('d/m/Y H:i')
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make()
+                    ->modalWidth(MaxWidth::Large),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageLevels::route('/'),
+        ];
+    }
+
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }
+}

c7643884a14a9014ded7776363fa3ae40fe2ef16
vitorbizarra <vitorbizarracontato@gmail.com>
2024-04-08 22:16:04 -0300

    :sparkles: feat: Relacionamento ManyToMany entre Album e Tierlist

--- app/Models/Ranking/Album.php
+++ app/Models/Ranking/Album.php
@@ -5,6 +5,7 @@ namespace App\Models\Ranking;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Album extends Model
@@ -21,5 +22,10 @@ class Album extends Model
     public function level(): BelongsTo
     {
         return $this->belongsTo(Level::class);
+    }
+
+    public function tierlists(): BelongsToMany
+    {
+        return $this->belongsToMany(Tierlist::class);
     }
 }
--- app/Models/Ranking/Tierlist.php
+++ app/Models/Ranking/Tierlist.php
@@ -4,6 +4,7 @@ namespace App\Models\Ranking;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Tierlist extends Model
@@ -14,4 +15,9 @@ class Tierlist extends Model
         'name',
         'sort',
     ];
+
+    public function albums(): BelongsToMany
+    {
+        return $this->belongsToMany(Album::class);
+    }
 }
